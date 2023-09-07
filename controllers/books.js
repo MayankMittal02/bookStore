d67d6d697d6fce4eaeee6439dc4589bb28447e2c
MayankMittal02 <140265467+MayankMittal02@users.noreply.github.com>
2023-09-07 23:58:20 +0530

    second version

--- controllers/books.js
+++ controllers/books.js
@@ -9,7 +9,7 @@ const getAllBooksStatic = async (req, res) => {
 
 
 
-    const book = await Book.find({ "title": "2nd book" })
+    const book = await Book.find({}).sort('publishYear -author')
     // book = await book.find()
 
     res.status(200).json({ book })
@@ -27,10 +27,10 @@ const getAllBooks = async (req, res) => {
     const queryObject = {}
 
     if (title) {
-        queryObject.title = {$regex : title , $options:'i'}
+        queryObject.title = { $regex: title, $options: 'i' }
     }
     if (author) {
-        queryObject.author = author
+        queryObject.author = { $regex: author, $options: 'i' }
     }
     if (publishYear) {
         queryObject.publishYear = publishYear
@@ -39,9 +39,22 @@ const getAllBooks = async (req, res) => {
         queryObject.inStock = true
     }
 
+    let result = Book.find(queryObject);
 
+    if (sort) {
+        const sortList = sort.split(',').join(' ');
+        result = result.sort(sortList);
+    } else {
+        result = result.sort('publishYear');
+    }
 
-    const book = await Book.find(queryObject)
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit);
+
+    const book = await result;
 
     res.status(200).json({ book })
 
@@ -82,9 +95,7 @@ const updateBook = async (req, res) => {
     res.status(200).json({ book })
 }
 
-// module.exports = {
-//     getAllBooksStatic,getAllBooks, getBook, insertBook, deleteBook, updateBook
-// }
+
 
 module.exports = {
     getAllBooksStatic, getAllBooks, getBook, insertBook, deleteBook, updateBook
